1db11772a63bb2fbb2a7306c3a023fe4c27cef9d
“zed” <zed@kolk.me>
2021-01-17 15:04:24 -0600

    finished

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self[-1] == '.'
+    false
   end
 
   def question?
-
+    return true if self[-1] == '?'
+    false
   end
 
   def exclamation?
-
+    return true if self[-1] == '!'
+    false
   end
 
   def count_sentences
-
+    self.split(Regexp.union('.','?','!')).count{|x| x.length != 0}
+    # self.split.select{|string| string.sentence? or string.question? or string.exclamation?}.length
   end
 end
