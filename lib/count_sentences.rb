a2b75198d3cd696beddab33c6f620292c41d0ab9
“zed” <zed@kolk.me>
2021-01-17 18:48:12 -0600

    commit tree 5bf2befb219f568f52054fd85b58c37d60f1eba5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,22 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    return true if self[-1] == '.'
-    false
+    # binding.pry
+    self[-1] == '.' ? true : false
+    # return true if self[-1] == '.'
+    # false
   end
 
   def question?
-    return true if self[-1] == '?'
-    false
+    self[-1] == '?' ? true : false
+    # return true if self[-1] == '?'
+    # false
   end
 
   def exclamation?
-    return true if self[-1] == '!'
-    false
+    # return true if self[-1] == '!'
+    # false
+    self[-1] == '!' ? true : false
+
   end
 
   def count_sentences
+
     self.split(Regexp.union('.','?','!')).count{|x| x.length != 0}
     # self.split.select{|string| string.sentence? or string.question? or string.exclamation?}.length
+
+    # def count_sentences
+    #   array = []
+    #   self.split(Regexp.union('.','?','!')).each do |sentence|
+    #     if sentence.length != 0
+    #       array << sentence
+    #     end
+    #   end
+    #   array.length
+    # end
+
   end
 end
